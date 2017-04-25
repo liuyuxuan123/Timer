6027b21b3420ec5d45bfd2139827a1fd4ef6269b
liuyuxuan123 <1010134430@qq.com>
2017-04-25 16:41:25 +0800

    Initial Commit

---
+++ Timer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Timer
+//
+//  Created by 刘宇轩 on 25/04/2017.
+//  Copyright © 2017 liuyuxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Timer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Timer
+//
+//  Created by 刘宇轩 on 25/04/2017.
+//  Copyright © 2017 liuyuxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
